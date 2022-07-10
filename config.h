ab717d3e2b11ea3b20eb12f5f4f69d45d587e8a5
Karen Gozeyan <kargozeyan@gmail.com>
2022-07-10 21:29:27 +0400

    color change

--- config.h
+++ config.h
@@ -3,10 +3,10 @@ static const char *user  = "karen";
 static const char *group = "karen";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#005577",   /* during input */
-	[FAILED] = "#CC3333",   /* wrong password */
+	[INIT] =   "#1e1e2e",     /* after initialization */
+	[INPUT] =  "#89b4fa",   /* during input */
+	[FAILED] = "#f38ba8",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
-static const int failonclear = 1;
+static const int failonclear = 0;
